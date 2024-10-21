fcaadb67110336ee5cd5459d3007186f53ea42c6
chahine elati <chahinosaiyan@gmail.com>
2024-10-21 07:44:39 +0100

    changer le titre de page courante

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,5 +8,33 @@ import { Component } from '@angular/core';
 export class AppComponent {
   title = 'assistant_medical_intelligent_front';
   authenticated = true;
-  constructor() {}
+  pageTitle: String = "Accueil";
+  navigation = [
+    {
+      page: "Accueil",
+      link: "/",
+    },
+    {
+      page: "Action",
+      link: "/action",
+    },
+    {
+      page: "Rendez-vous",
+      link: "/rendez-vous",
+    },
+    {
+      page: "Historique",
+      link: "/historique",
+    },
+    {
+      page: "Profil",
+      link: "/profil",
+    }
+  ]
+
+  constructor() { }
+
+  setPageTitle(title: String) {
+    this.pageTitle = title;
+  }
 }
