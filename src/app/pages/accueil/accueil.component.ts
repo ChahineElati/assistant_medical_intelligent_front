52ec32a6c53290f0037514ac7cd80ba550825520
chahine elati <chahinosaiyan@gmail.com>
2024-11-24 18:57:07 +0100

    modifier interface accueil calendrier

--- src/app/pages/accueil/accueil.component.ts
+++ src/app/pages/accueil/accueil.component.ts
@@ -1,10 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-accueil',
   templateUrl: './accueil.component.html',
-  styleUrls: ['./accueil.component.css']
+  styleUrls: ['./accueil.component.css'],
+  encapsulation: ViewEncapsulation.None,
+
 })
 export class AccueilComponent {
 
+  dates = [
+    new Date("2024-11-17"),
+    new Date("2024-11-16"),
+    new Date("2024-11-18"),
+    new Date("2024-11-19"),
+    new Date("2024-11-15"),
+  ];
+
+  dateClass = (date: Date): string => {
+    const highlight = this.dates.some(
+      d => d.getFullYear() === date.getFullYear() &&
+        d.getMonth() === date.getMonth() &&
+        d.getDate() === date.getDate()
+    );
+    return highlight ? 'rendez-vous' : '';
+  };
 }
